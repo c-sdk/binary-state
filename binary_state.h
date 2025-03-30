e8c8ce89cf7b1082078c2145850aab355b107058
Bruno Dias <dias.h.bruno@gmail.com>
2025-03-30 16:35:08 -0300

    added functions to toggle to an specific state.

--- binary_state.h
+++ binary_state.h
@@ -11,6 +11,10 @@ typedef enum binary_state_t switchable_t;
 typedef enum binary_state_t activatable_t;
 typedef enum binary_state_t enableable_t;
 
+static inline void toggle_on(enum binary_state_t *b) { *b = BINARY_ON; }
+
+static inline void toggle_off(enum binary_state_t *b) { *b = BINARY_OFF; }
+
 static inline void toggle(enum binary_state_t *b) {
   *b = (*b == BINARY_ON) ? BINARY_OFF : BINARY_ON;
 }
